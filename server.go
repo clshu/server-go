e569cd3858e5d74e09f06289b4830fac3fe729d7
clshu <clshu2020@gmail.com>
2021-06-03 03:41:32 -0700

    Change the route/path

--- server.go
+++ server.go
@@ -12,18 +12,23 @@ import (
 )
 
 const defaultPort = "8080"
+const defaultPath = "tvu_graphql"
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
+	gqlPath := os.Getenv("GRAPHQL_PATH")
+	if gqlPath == "" {
+		gqlPath = defaultPath
+	}
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &resolver.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	http.Handle("/tvu_playground", playground.Handler("GraphQL playground", `/${gqlPath}`))
+	http.Handle(`/${gqlPath}`, srv)
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Printf("connect to http://localhost:%s/tvu_playground for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
